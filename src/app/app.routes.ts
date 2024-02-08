d783da6c3c3ea15c45c18d04525d6e75ba274eca
Smruti Ranjan Rana <smrutiranjan.developer@gmail.com>
2024-02-08 05:59:06 +0000

    feat: add product list in router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        redirectTo: '/products',
+        pathMatch: "full"
+    },
+    {
+        path: 'products',
+        loadComponent: () => import("./product-list/product-list.component").then(c => c.ProductListComponent)
+    }
+];
